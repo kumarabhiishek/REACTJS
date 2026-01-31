a76e0eb4648fdfe4f13ac0bf520cfc1fbf71e052
kumarabhiishek <kumarabhiishek17@gmail.com~>
2026-01-31 15:24:38 +0530

    react router

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-xl text-center'>Currency Converter</div>
+  )
+}
+
+export default App
